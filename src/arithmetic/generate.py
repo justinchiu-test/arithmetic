6baec1db7b962969e5edfe9f7bc82b8245bcb161
Justin Chiu <justinchiu@cohere.com>
2024-12-02 06:00:46 +0000

    generate

---
+++ src/arithmetic/generate.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+def main(
+    num_examples: int = 20_000,
+    min_value: int = 1_000,
+    max_value: int = 10_000,
+    num_values: int = 2,
+    operations: list[str] = ["+"],
+):
+    pass
+
+
+if __name__ == "__main__":
+    from strictfire import StrictFire
+    StrictFire(main)
