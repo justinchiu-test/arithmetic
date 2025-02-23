992e1cb2f18d157700343f0f668c375e7a3de545
justinchiu-cohere <justinchiu@cohere.com>
2025-02-23 13:05:37 -0500

    increase values

--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ do_fewshot=True
 for i in 3 5 7 9
 do
     # training set of 100k
-    uv run python -m arithmetic.generate --num_examples 100000 --min_value 10_000 --max_value 100_000 --num_values $i --do_generate $do_generate --do_promptify $do_promptify --do_fewshot $do_fewshot
+    #uv run python -m arithmetic.generate --num_examples 100000 --min_value 10_000 --max_value 1_000_000 --num_values $i --do_generate $do_generate --do_promptify $do_promptify --do_fewshot $do_fewshot
     # training set of 50k
-    uv run python -m arithmetic.generate --num_examples 50000 --min_value 10_000 --max_value 100_000 --num_values $i --do_generate $do_generate --do_promptify $do_promptify --do_fewshot $do_fewshot
+    #uv run python -m arithmetic.generate --num_examples 50000 --min_value 10_000 --max_value 1_000_000 --num_values $i --do_generate $do_generate --do_promptify $do_promptify --do_fewshot $do_fewshot
     # training set of 20k
-    uv run python -m arithmetic.generate --num_examples 20000 --min_value 10_000 --max_value 100_000 --num_values $i --do_generate $do_generate --do_promptify $do_promptify --do_fewshot $do_fewshot
+    uv run python -m arithmetic.generate --num_examples 20000 --min_value 10_000 --max_value 1_000_000 --num_values $i --do_generate $do_generate --do_promptify $do_promptify --do_fewshot $do_fewshot
     # test set of 10k
-    uv run python -m arithmetic.generate --num_examples 10000 --min_value 10_000 --max_value 100_000 --num_values $i
+    uv run python -m arithmetic.generate --num_examples 10000 --min_value 10_000 --max_value 1_000_000 --num_values $i
 done
 
